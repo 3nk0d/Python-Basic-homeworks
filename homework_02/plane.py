414a8560ef5512403c394bc14dcf2d87871ec60b
3nk0d <D1man680@yandex.com>
2022-10-25 21:49:29 +0300

    Add class plane, load_cargo, remove_all_cargo in plane.py

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,25 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    cargo: int = 0
+    max_cargo: int
+
+    def __init__(self,max_cargo = 50):
+        self.max_cargo = max_cargo
+
+    def load_cargo(self,cargo):
+        weight = cargo + self.cargo
+        if weight <= self.max_cargo:
+            self.cargo = weight
+        raise CargoOverload
+
+    def remove_all_cargo(self):
+        tmp = self.cargo
+        self.cargo = 0
+        return tmp
+
