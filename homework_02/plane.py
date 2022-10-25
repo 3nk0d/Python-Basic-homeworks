93834c4fcf5049187972e5c50a67e5691f9b9939
3nk0d <D1man680@yandex.com>
2022-10-25 22:22:47 +0300

    Add eight, started, fuel, fuel_consumption attributes in __init__ of Plane in plane.py Minor changes in name in plane.py

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -9,10 +9,13 @@ class Plane(Vehicle):
     cargo: int = 0
     max_cargo: int
 
-    def __init__(self,max_cargo = 50):
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo = 50):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
         self.max_cargo = max_cargo
 
-    def load_cargo(self,cargo):
+    def load_cargo(self, cargo):
         weight = cargo + self.cargo
         if weight <= self.max_cargo:
             self.cargo = weight
