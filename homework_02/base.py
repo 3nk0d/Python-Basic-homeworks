7782adaabca681cf7cb64ed653c4936c2c86cb5e
3nk0d <D1man680@yandex.com>
2022-10-25 21:40:19 +0300

    Change Vehicle, add start and move in base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,27 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    weight: int = 60
+    started: bool = False
+    fuel: int = 30
+    fuel_consumtion: int = 1
+
+    def __init__(self, weight, started , fuel, fuel_consumtion):
+        self.weight = weight
+        self.started = started
+        self.fuel = fuel
+        self.fuel_consumtion = fuel_consumtion
+
+    def start(self):
+        if not(self.started):
+            if self.fuel > 0:
+                self.started = True
+            raise LowFuelError
+
+    def move(self, distance):
+        fuel_exist = self.fuel - (distance * self.fuel_consumtion)
+        if fuel_exist >= 0:
+            self.fuel = fuel_exist
+        raise NotEnoughFuel
