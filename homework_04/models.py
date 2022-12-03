97fdd7ac040eef566b885d8b85c07e5ebd8cef66
Enk0d <D1man680@yandex.com>
2022-12-04 01:56:37 +0300

    Add User, Post classes

--- homework_04/models.py
+++ homework_04/models.py
@@ -7,10 +7,11 @@
 для модели Post обязательными являются user_id, title, body
 создайте связи relationship между моделями: User.posts и Post.user
 """
-
 import os
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
@@ -28,5 +29,15 @@ Session = sessionmaker(
 )
 
 
+class User(Base):
+    name = Column(String(30))
+    username = Column(String(20), unique=True)
+    email = Column(String(35))
+    posts = relationship('Post', back_populates='User')
 
 
+class Post(Base):
+    user_id = Column(Integer)
+    title = Column(String)
+    body = Column(String)
+    user = relationship('User', back_populates='Post')
