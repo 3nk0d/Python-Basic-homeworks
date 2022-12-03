d8eb7d5333fce44bc46450fa50d984933a807601
Enk0d <D1man680@yandex.com>
2022-12-04 02:08:41 +0300

    Add users_get and posts_get func

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -6,8 +6,17 @@ import aiohttp
 USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users/"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts/"
 
+
 async def fetch_json(URL):
     async with aiohttp.ClientSession() as session:
         async with session.get(URL) as resp:
             print(resp.status)
             print(await resp.text())
+
+
+async def users_get():
+    await fetch_json(USERS_DATA_URL)
+
+
+async def posts_get():
+    await fetch_json(POSTS_DATA_URL)
