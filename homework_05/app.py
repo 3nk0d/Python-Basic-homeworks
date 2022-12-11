edc3b5a4729ec47f034bed26e36ed82e53766669
Enk0d <D1man680@yandex.com>
2022-12-11 22:51:13 +0300

    Add index, about

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,17 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return "<h1>Index page:<h1>"
+
+@app.get("/about/")
+def about():
+    return "<body><h1>About page<h1><br><h3>This site is made for homework 05.</h3></body>"
+
