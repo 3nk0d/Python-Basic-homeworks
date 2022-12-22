d8e0f90187e56a51a35c55d9d7b60cb609af12f2
Enk0d <D1man680@yandex.com>
2022-12-23 02:00:13 +0300

    Add filling DB

--- homework_06/app.py
+++ homework_06/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+from fill_db import main, Session
 
+main()
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/create_records/")
 def create_records():
     return render_template("create_records.html")
 
+
+@app.route("/view_records/")
+def view_records():
+    return render_template("view_records.html", record = "record_1")
