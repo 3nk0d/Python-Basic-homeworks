24aad21b2fc26d26c38551858ad3e7a134e3fb25
Enk0d <D1man680@yandex.com>
2022-12-23 00:52:33 +0300

    Add index page

--- homework_06/app.py
+++ homework_06/app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    render_template("index.html")
 
 @app.route("/create_records/")
 def create_records():
     return render_template("create_records.html")
+
