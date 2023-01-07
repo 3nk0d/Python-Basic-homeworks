b2780a3b443836aca3d992c5fa68395728510182
Enk0d <D1man680@yandex.com>
2023-01-07 04:18:40 +0300

    Created models

--- homework_07/news/view_news/models.py
+++ homework_07/news/view_news/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.CharField(max_length=50)
+    url = models.URLField()
+
+
+class Users(models.Model):
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+class Req_Urls(models.Model):
+    name = models.CharField(max_length=40)
+    url = models.URLField()
