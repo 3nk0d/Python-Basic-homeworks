85fe740b8296b68153faf1aa86568f635971a03a
Enk0d <D1man680@yandex.com>
2023-01-10 01:50:48 +0300

    Add spaces

--- homework_08/news/view_news/views.py
+++ homework_08/news/view_news/views.py
@@ -10,23 +10,30 @@ def main_page(request):
 class Posts_ListView(ListView):
     model = Posts
 
+
 class Posts_DetailView(DetailView):
     model = Posts
+
 
 class Users_ListView(ListView):
     model = Users
 
+
 class Users_DetailView(DetailView):
     model = Users
+
 
 class Req_Urls_ListView(ListView):
     model = Req_Urls
 
+
 class Req_Urls_DetailView(DetailView):
     model = Req_Urls
 
+
 class Tags_ListView(ListView):
     model = Tags
+
 
 class Tags_DetailView(DetailView):
     model = Tags
