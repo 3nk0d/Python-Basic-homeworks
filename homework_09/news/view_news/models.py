207562d1cf4139bba2247e95115d136964c9b231
Enk0d <D1man680@yandex.com>
2023-01-18 01:59:10 +0300

    Add __str__ to models for views

--- homework_09/news/view_news/models.py
+++ homework_09/news/view_news/models.py
@@ -6,10 +6,16 @@ class Users(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
 
+    def __str__(self):
+        return f'{self.username} : {self.email}'
+
 
 class Req_Urls(models.Model):
     name = models.CharField(max_length=40, unique=True)
     url = models.URLField()
+
+    def __str__(self):
+        return f'{self.name} : {self.url}'
 
 
 class Posts(models.Model):
