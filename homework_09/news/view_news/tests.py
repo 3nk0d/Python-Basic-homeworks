21dedbe7c909fac6ada4146c7dbdb0687c91a61c
Enk0d <D1man680@yandex.com>
2023-01-18 00:00:46 +0300

    Add first test

--- homework_09/news/view_news/tests.py
+++ homework_09/news/view_news/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Users, Req_Urls, Posts, Tags
 # Create your tests here.
+
+class TestUsers(TestCase):
+
+    def setUp(self) -> None:
+        self.user = Users.objects.create(username="link", name="James", email="Jam2@mail.com")
+
+    def tearDown(self) -> None:
+
+
+    def CheckData(self):
+        self.assertEqual(self.user.username, "link")
+        self.assertEqual(self.user.name, "James")
+        self.assertEqual(self.user.email, "Jam2@mail.com")
+
