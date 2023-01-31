bed2bd6532beef4724b3de955d2756645891f3db
Chumachenko Dmitry <D1man680@yandex.com>
2023-01-31 20:47:20 +0300

    Create main.py

---
+++ homework_10/main.py
@@ -0,0 +1,52 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*args):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+    return [num ** 2 for num in args]
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def is_prime(num):
+    """
+    Функция дляпроверки одного числа
+    Простое ли это число.    
+    """
+    if num in (0, 1):
+        return False
+    elif num == 2:
+        return True
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def filter_numbers(lst, filtr):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    if filtr == ODD:
+        return [i for i in lst if i % 2]
+    elif filtr == EVEN:
+        return [i for i in lst if not(i % 2)]
+    return list(filter(is_prime, lst))
